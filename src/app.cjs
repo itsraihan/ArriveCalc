b1aa1ca1562e5508164340a047a05c11dcd3aa5c
itsraihan <raihanfajari44@gmail.com>
2024-10-27 19:46:40 +0700

    Config: Add not found middleware

--- src/app.cjs
+++ src/app.cjs
@@ -1,13 +1,15 @@
-const express = require('express');
-const { default: timeRouter } = require('./routers/time.router.cjs');
-const { default: durationRouter } = require('./routers/duration.router.cjs');
+const express = require("express");
 const app = express();
-const router = require('./routers/index.router.cjs');
+const router = require("./routers/index.router.cjs");
 
 // Middleware untuk parsing body JSON
 app.use(express.json());
 
-app.use('/api', router);
+app.use("/api", router);
+
+app.use((_req, res, _next) => {
+    res.status(404).json({ message: "Resource not found" });
+});
 
 // Jalankan server
 const PORT = process.env.PORT || 3000;
